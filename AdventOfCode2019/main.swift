38fe4e81fe2d666032d39f102c3a9ec2f5ea6dae
Raul <raulferrodrigues@gmail.com>
2019-12-03 13:53:43 -0300

    Initial Commit

---
+++ AdventOfCode2019/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode2019
+//
+//  Created by Raul Rodrigues on 12/3/19.
+//  Copyright © 2019 Raul Rodrigues. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
